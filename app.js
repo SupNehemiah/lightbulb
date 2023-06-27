405835bea9a62114c300ceb06bc73042a2a9b9a1
TXT-87 <txt.hackathon@TXT-87.lan>
2023-06-26 21:05:46 -0700

    lightbulb

---
+++ app.js
@@ -0,0 +1,8 @@
+var img = document.getElementById ("lightbulb");
+    function off(){
+            img.src="https://www.nicepng.com/png/detail/8-85199_light-bulb-on-off-png-stock-light-bulb.png";
+    }
+    function on(){
+            img.src="https://www.hometownevolutioninc.com/wp-content/uploads/2019/04/A15_clear_light_bulbs_E26_commercial.jpg";
+            
+    }
